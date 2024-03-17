cc959e818fb57a6f86aa099d8706ccf3841f0c4c
Bohdan <154711952+7GitGuru@users.noreply.github.com>
2024-03-18 01:34:43 +0200

    🧑‍💻 Update to release v2.0.1

--- main.py
+++ main.py
@@ -1,20 +1,70 @@
 import time
-from api_handling import get_crypto_price
-from telegram_bot import send_message
 import configparser
+from api import *
+from telegram_bot import send_message
 
 config = configparser.ConfigParser()
 config.read('config/config.ini')
-tracked_coins = config['Cryptocurrencies']['tracked_coins']
+tracked_coins = config['Cryptocurrencies']['cryptocurrency']
+sleep_interval = int(config['Settings']['sleep_interval'])
+exchanges_config = config['Exchanges']
 
+previous_prices = {}
+
+
+# Here you can find a list of all cryptocurrencies you can use:
+# https://github.com/7GitGuru/crypto-tracker/blob/main/coin-names.json
 
 def main():
     while True:
         for coin in tracked_coins.split(','):
-            price = get_crypto_price(coin)
-            message = f"{coin.capitalize()} price is ${price}"
-            send_message(message)
-        time.sleep(15)
+            try:
+                prices = {}
+                message = f"{coin.capitalize()} prices:\n"
+                if exchanges_config.getboolean('binance'):
+                    try:
+                        price_binance = Binance(coin)
+                        prices["Binance"] = price_binance
+                    except Exception as e:
+                        print(f"Binance error: {e}")
+                if exchanges_config.getboolean('bybit'):
+                    try:
+                        price_bybit = Bybit(coin)
+                        prices["Bybit"] = price_bybit
+                    except Exception as e:
+                        print(f"Bybit error: {e}")
+                if exchanges_config.getboolean('bitfinex'):
+                    try:
+                        price_bitfinex = Bitfinex(coin)
+                        prices["Bitfinex"] = price_bitfinex
+                    except Exception as e:
+                        print(f"Bitfinex error: {e}")
+                if exchanges_config.getboolean('coinbase'):
+                    try:
+                        price_coinbase = Coinbase(coin)
+                        prices["Coinbase"] = price_coinbase
+                    except Exception as e:
+                        print(f"Coinbase error: {e}")
+
+                if len(prices) > 0:
+                    for exchange, price in prices.items():
+                        previous_price = previous_prices.get(coin, {}).get(exchange)
+                        if previous_price is None:
+                            emoji = "💲"
+                        elif previous_price < price:
+                            emoji = "📈"
+                        elif previous_price > price:
+                            emoji = "📉"
+                        else:
+                            emoji = "💲"
+                        message += f"{emoji}{exchange}: ${price}\n"
+                        previous_prices.setdefault(coin, {})[exchange] = price
+                    send_message(message)
+                else:
+                    print(f"No exchanges available for {coin}")
+            except Exception as e:
+                print(f"Error: {e}")
+        time.sleep(sleep_interval)
 
 
 if __name__ == "__main__":
